26c47c6d2ee0f9a22603e8275a63ef535e6a455f
GrooshBene <wltn000129@gmail.com>
2017-02-13 21:57:02 +0900

    db.js connectDB function added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server = require('./route/server');
 
 server.openServer(8000);
 
-server.getPostReq('/', function(req, res){
+server.catchPostReq('/', function(req, res){
     console.log("req : " + req.param('asdf'));
     res.send("asdf");
 })
--- route/db.js
+++ route/db.js
@@ -1,3 +1,14 @@
 /**
  * Created by GrooshBene on 2017-02-13.
  */
+var mongoose = require('mongoose');
+var randomString = require('randomString');
+
+function connectDB(url){
+    mongoose.connect(url, function(err){
+        if(err){
+            console.log("Mongo DB Connection Error!");
+            throw err;
+        }
+    })
+}
