f78ccc4a0dcd74f28a90a08a9d5eb63f3cf3d295
GrooshBene <wltn000129@gmail.com>
2017-02-18 21:04:51 +0900

    gitignore file added, package.json added

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
  * Created by GrooshBene on 2017-02-13.
  */
 var server = require('./route/server');
+var social = require('./route/social');
 
 server.openServer(8000);
 
 server.catchPostReq('/', function(req, res){
     console.log("req : " + req.param('asdf'));
     res.send("asdf");
-})
+});
+
+social.initialize();
+social.fbTokeninit("1132996196786425", "6d689678c191d56a3f8dfcae932a9fac", function (result) {
+    console.log(result);
+});
 //===================================================
