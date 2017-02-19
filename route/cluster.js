3f6881153374eaf7c512e7fa83cb585e693f6b25
GrooshBene <wltn000129@gmail.com>
2017-02-19 22:01:56 +0900

    cluster.js forkCluster function Added

--- route/cluster.js
+++ route/cluster.js
@@ -1,3 +1,17 @@
 /**
  * Created by GrooshBene on 2017-02-19.
  */
+var cluster = require('cluster');
+var http = require('http');
+var CPUNum = require('os').cpus().length;
+
+function forkCluster(){
+    for(var i=0; i < CPUNum; i++){
+        cluster.fork();
+    }
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('worker' + worker.process.pid + '가 종료되었습니다.');
+    });
+}
+
+exports.forkCluster = forkCluster;
