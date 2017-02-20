f81fc5988b85d437e06e24b7203ccdc5a4d39218
GrooshBene <wltn000129@gmail.com>
2017-02-20 22:39:03 +0900

    args.js added, params object making done

---
+++ route/args.js
@@ -0,0 +1,19 @@
+/**
+ * Created by GrooshBene on 2017-02-20.
+ */
+var argv = require('argv');
+
+var CONST = require('./constants.js');
+
+argv.option(CONST.OPTIONS);
+argv.version(CONST.VERSION);
+argv.info(CONST.DESCRIPTION);
+
+var arguments = argv.run();
+var params = new Object();
+
+params.interface = arguments.options.interface || 'any';
+params.filter = arguments.options.filter || 'ip proto \\tcp';
+params.log = arguments.options.log;
+
+exports.params = params;
